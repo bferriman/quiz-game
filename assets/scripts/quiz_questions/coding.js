8a14fb04358b85a551bb41611708a7ad4ab92b20
Ben F <56423163+bferriman@users.noreply.github.com>
2019-12-01 00:27:01 -0500

    added question content

---
+++ assets/scripts/quiz_questions/coding.js
@@ -0,0 +1,63 @@
+var questions = [
+
+    {
+        title: "Which of the following is NOT a way to insert CSS?",
+        choices: ["Internal", "External", "Inline", "Outline"],
+        answer: "Outline"
+    },
+
+    {
+        title: "Which of the following is NOT a potential variable scope in JavaScript?",
+        choices: ["Global", "Block", "Header", "Function"],
+        answer: "Header"
+    },
+
+    {
+        title: "Which of the following is a strict comparison?",
+        choices: ["===", "<=", "!=", "=="],
+        answer: "==="
+    },
+
+    {
+        title: "What function can be used to convert a string to a number?",
+        choices: ["toNum()", "Number()", "convertString()", "strToNum()"],
+        answer: "Number()"
+    },
+
+    {
+        title: "What is NOT a potential return value of the 'typeof' operator?",
+        choices: ["loop", "number", "object", "boolean"],
+        answer: "loop"
+    },
+
+    {
+        title: "The CSS declaration 'margin: 10px 20px 30px 40px' will result in what margins?",
+        choices: ["top: 10px, bottom: 20px, left: 30px, right: 40px", "top: 10px, right: 20px, bottom: 30px, left: 40px", "left: 10 px, right: 20px, top: 30px, bottom: 40px", "top: 10px, left: 20px, bottom: 30px, right: 40px"],
+        answer: "top: 10px, right: 20px, bottom: 30px, left: 40px"
+    },
+
+    {
+        title: "What is the correct order of the components of the box model?"
+        choices: ["content, border, padding, margin", "content, margin, border, padding", "content, padding, margin, border", "content, padding, border, margin"],
+        answer: "content, padding, border, margin"
+    },
+
+    {
+        title: "Which of the following is NOT a valid way to specify a color in CSS?",
+        choices: ["RGB value", "HEX value", "binary value", "color name"],
+        answer: "binary value"
+    },
+
+    {
+        title: "Which of the following tags is self-closing?",
+        choices: ["<p>", "<img>", "<div>", "<a>"],
+        answer: "<img>"
+    },
+
+    {
+        title: "What is the correct way to reference an element with the 'testClass' class in CSS?",
+        choices: ["testClass", "#testClass", ".testClass", "$testClass"],
+        answer: ".testClass"
+    }
+
+];
---
+++ assets/scripts/quiz_questions/geography.js
@@ -0,0 +1,63 @@
+var questions = [
+
+    {
+        title: "Which of the following countries is on the Iberian Peninsula?",
+        choices: ["Greece", "Turkey", "Spain", "Norway"],
+        answer: "Spain"
+    },
+
+    {
+        title: "Which of the following countries is landlocked?",
+        choices: ["Latvia", "Iraq", "Uruguay", "Zambia"],
+        answer: "Zambia"
+    },
+
+    {
+        title: "What is the former name of Eswatini?",
+        choices: ["Swaziland", "Zanzibar", "Rhodesia", "Zaire"],
+        answer: "Swaziland"
+    },
+
+    {
+        title: "In what state would you find Glacier National Park?",
+        choices: ["Colorado", "Montana", "Alaska", "Hawaii"],
+        answer: "Montana"
+    },
+
+    {
+        title: "Which of the following rivers is the shortest?",
+        choices: ["Nile", "Niger", "Congo", "Zambezi"],
+        answer: "Zambezi"
+    },
+
+    {
+        title: "After Virginia, which state is the birthplace of the most U.S. presidents?",
+        choices: ["Ohio", "Massachusetts", "Pennsylvania", "New York"],
+        answer: "Ohio"
+    },
+
+    {
+        title: "Which of the following is one of the Seven Wonders of the Ancient World?",
+        choices: ["Leaning Tower of Pisa", "Lighthouse of Alexandria", "Stonehenge", "Colosseum"],
+        answer: "Lighthouse of Alexandria"
+    },
+
+    {
+        title: "Which of the following countries touches the equator?",
+        choices: ["Chad", "Thailand", "Colombia", "Philippines"],
+        answer: "Colombia"
+    },
+
+    {
+        title: "Which of the following states has the largest population?",
+        choices: ["Missouri", "Minnesota", "Mississippi", "Michigan"],
+        answer: "Michigan"
+    },
+
+    {
+        title: "Which of the following is not a state capital?",
+        choices: ["Philadelphia", "Boston", "Denver", "Phoenix"],
+        answer: "Philadelphia"
+    }
+    
+];
